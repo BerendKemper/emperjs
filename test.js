c0e40b3e49cfc44c2ba04cd1b7235bc5bf380e6f
Berend Kemper <berend_kemper@hotmail.com>
2020-10-06 14:20:42 +0200

    test incomming request on errors

--- test.js
+++ test.js
@@ -1,32 +1,32 @@
-fetch("/test", {
-    method: "PUT",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ a: 5 })
+fetch("/test", { // No Errors
+	method: "PUT",
+	headers: { "content-type": "application/json" },
+	body: JSON.stringify({ a: 5 })
 }).then(response => {
-    console.log(response);
-    return response.text();
+	console.log(response);
+	return response.text();
 }).then(result => {
-    console.log(result);
+	console.log(result);
 });
 
 fetch("/test", {
-    method: "PUT",
-    headers: { "content-type": "appliication/json" },
-    body: JSON.stringify({ a: 5 })
+	method: "PUT",
+	headers: { "content-type": "appliication/json" }, // Error! apli- i -cation/json
+	body: JSON.stringify({ a: 5 })
 }).then(response => {
-    console.log(response);
-    return response.text();
+	console.log(response);
+	return response.text();
 }).then(result => {
-    console.log(result);
+	console.log(result);
 });
 
 fetch("/test", {
-    method: "PUT",
-    headers: { "content-type": "application/json" },
-    body: "JSON.stringify({ a: 5 })"
+	method: "PUT",
+	headers: { "content-type": "application/json" },
+	body: "JSON.stringify({ a: 5 })" // Error! body not JSON while content-type is application/json
 }).then(response => {
-    console.log(response);
-    return response.text();
+	console.log(response);
+	return response.text();
 }).then(result => {
-    console.log(result);
+	console.log(result);
 });
