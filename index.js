84c39f04ecfcd02984d583a5f49b2cb7fa8b9ad0
Berend Kemper <berend_kemper@hotmail.com>
2020-10-15 23:39:27 +0200

    app api statistics module

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 "use strict";
-const http = require("http"); // console.log(http.STATUS_CODES); console.log(http.METHODS);
+const http = require("http");
+/* console.log(http.STATUS_CODES); console.log(http.METHODS); */
 const App = require("./lib/app");
 
 let app = new App(http);
-// console.log(app.requestDataParsers);
-app.listen(8080);
+/* console.log(app.requestDataParsers); */
+app.loadApiStats({
+	"/": {
+		"GET": {
+			"counter": 10,
+			"bytes": 500
+		}
+	},
+	"/test/:mongol/test": {
+		"GET": {
+			"counter": 15,
+			"bytes": 2500
+		}
+	}
+});
 app.get("/", (request, response) => {
-	console.log(request.socket.server);
-	console.log(Object.getOwnPropertyDescriptors(request.socket.server._events.connection[0].prototype));
 	response.bye("IT WORKED AHAHAHA");
+	console.log(request.stats);
 });
 app.get("/test/:mongol/test", (request, response) => {
-	console.log(request.api)
 	response.bye("IT WORKED! AHAHAHA params.mongol = " + request.params.mongol);
+	console.log(request.stats);
+	request.stats.counter++;
 });
 app.get("/test/:monkey", (request, response) => {
-
+	response.bye("IT WORKED AHAHAHA");
 });
 app.put("/test/:monk/notzen", (request, response) => {
-
+	response.bye("IT WORKED AHAHAHA");
 });
+app.listen(8080);
