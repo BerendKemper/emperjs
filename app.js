82ff4ca242bc1cac9d35c58b7e630831932b898a
Berend <berend_kemper@hotmail.com>
2021-10-10 14:45:23 +0200

    typescript with d.ts replaces jsDoc

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const RequestFactory = require("./lib/request");
 const ResponseFactory = require("./lib/response");
 const SocketFactory = require("./lib/socket");
 const ApiRegisterFactory = require("./lib/apiRegister");
+const _mimetypes = require("emperjs/lib/fileTypes");
 const Logger = require("./lib/logger");
 const Routes = require("./lib/routes");
 const isDerived = require("is-derived");
-const _mimetypes = require("./lib/fileTypes");
+function defaultListener() {
+    console.log(`Listening on: ${this.url}`);
+}
+/**@type {import("emperjs/emper").AppFactory}*/
 module.exports = (protocol, options) => {
     var http = require("http");
     protocol === "https" ? http = require("https") : protocol = "http";
@@ -21,20 +25,18 @@ module.exports = (protocol, options) => {
     const { onconnection } = Socket;
     function onListening() {
         this._handle.onconnection = onconnection;
-    };
+    }
     function onRequest(request, response) {
         logger?.log(request.method, "url: " + request.url);
-    };
+    }
     /* function onConnection(socket) { }; */
     /* function onError(error) { }; */
     const ApiRegister = ApiRegisterFactory();
     const apiRegister = new ApiRegister();
     let app = false;
     return class App extends http.Server {
-        /**@param {String} protocol @param {{insecureHTTPParser:boolean maxHeaderSize:number}} options**/
         constructor(options = {}) {
-            if (app === true) throw new Error("An App can only create one instance");
-            app = true;
+            if (app === (app = true)) throw new Error("An App can only create one instance");
             if (Object.prototype.toString.call(options) !== "[object Object]") throw new TypeError("param must be an object");
             options.IncomingMessage = EmperRequest;
             options.ServerResponse = EmperResponse;
@@ -44,47 +46,38 @@ module.exports = (protocol, options) => {
             this.once("listening", onListening);
             if (logger) this.on("request", onRequest);
         }
-        /**@param {{port:string hostname:string listeningListener:function}} options**/
-        listen(options = {}) {
-            let { port = 8080, hostname = "127.0.0.1", listeningListener = () => console.log(`Listening on: ${this.url}`) } = options;
-            super.listen(port, hostname, null, listeningListener);
+        listen(options = {}, listeningListener = defaultListener) {
+            const { hostname: host = "127.0.0.1", port = 8080, backlog = null } = options ?? {};
+            super.listen({ port, host, backlog }, listeningListener);
         }
-        /**@param {string} path @param {requestCallback} callback*/
         delete(path, callback) {
             if (typeof callback !== "function") throw new TypeError("Callback must be a function");
             routes.add(path, "DELETE", callback).apiRecord = apiRegister.register(path, "DELETE");
         }
-        /**@param {string} path @param {requestCallback} callback*/
         get(path, callback) {
             if (typeof callback !== "function") throw new TypeError("Callback must be a function");
             routes.add(path, "GET", callback).apiRecord = apiRegister.register(path, "GET");
         }
-        /**@param {string} path @param {requestCallback} callback*/
         head(path, callback) {
             if (typeof callback !== "function") throw new TypeError("Callback must be a function");
             routes.add(path, "HEAD", callback).apiRecord = apiRegister.register(path, "HEAD");
         }
-        /**@param {string} path @param {requestCallback} callback*/
         options(path, callback) {
             if (typeof callback !== "function") throw new TypeError("Callback must be a function");
             routes.add(path, "OPTIONS", callback).apiRecord = apiRegister.register(path, "OPTIONS");
         }
-        /**@param {string} path @param {requestCallback} callback*/
         patch(path, callback) {
             if (typeof callback !== "function") throw new TypeError("Callback must be a function");
             routes.add(path, "PATCH", callback).apiRecord = apiRegister.register(path, "PATCH");
         }
-        /**@param {string} path @param {requestCallback} callback*/
         post(path, callback) {
             if (typeof callback !== "function") throw new TypeError("Callback must be a function");
             routes.add(path, "POST", callback).apiRecord = apiRegister.register(path, "POST");
         }
-        /**@param {string} path @param {requestCallback} callback*/
         put(path, callback) {
             if (typeof callback !== "function") throw new TypeError("Callback must be a function");
             routes.add(path, "PUT", callback).apiRecord = apiRegister.register(path, "PUT");
         }
-        /**@param {object} register @param {boolean} reset*/
         loadApiRegister(register, reset) {
             if (Object.prototype.toString.call(register) !== "[object Object]") throw new TypeError("param must be an object");
             const apis = apiRegister.apis;
@@ -111,7 +104,6 @@ module.exports = (protocol, options) => {
         static get IncomingMessage() {
             return EmperRequest;
         }
-        /**Set this value to null in order to reset it to the base Request class.*/
         static set IncomingMessage(OwnIncomingMessage) {
             if (OwnIncomingMessage === null) return EmperRequest = Request;
             else if (!isDerived(OwnIncomingMessage, Request)) throw TypeError(`The parameter IncomingMessage is not derived from Request`);
@@ -120,7 +112,6 @@ module.exports = (protocol, options) => {
         static get ServerResponse() {
             return EmperResponse;
         }
-        /**Set this value to null in order to reset it to the base Response class.*/
         static set ServerResponse(OwnServerResponse) {
             if (OwnServerResponse === null) return EmperResponse = Response;
             else if (!isDerived(OwnServerResponse, Response)) throw TypeError(`The parameter ServerResponse is not a child of Response`);
@@ -135,7 +126,6 @@ module.exports = (protocol, options) => {
         static get ApiRecord() {
             return ApiRegister.ApiRecord;
         }
-        /**Set this value to null in order to reset it to the base ApiRecord class.*/
         static set ApiRecord(OwnApiRecord) {
             ApiRegister.ApiRecord = OwnApiRecord;
         }
@@ -145,7 +135,6 @@ module.exports = (protocol, options) => {
         static get mimetypes() {
             return _mimetypes;
         }
-        /**Add mimetypes to the dictionary. The mimetypes enables detecting the content-type by the extension from a file and is used in the response's sendFile method*/
         static set mimetypes(mimetypes) {
             if (Object.prototype.toString.call(mimetypes) !== "[object Object]") throw new TypeError("param must be an object");
             for (const type in mimetypes)
@@ -153,9 +142,3 @@ module.exports = (protocol, options) => {
         }
     };
 };
-try {
-    /* This part gives you intellisense on requests and responses because the code thinks it gets the classes. However it only stores null in these two variables */
-    var emperReuqest = new (RequestFactory(null))();
-    var emperResponse = new (ResponseFactory(null))();
-} catch (e) { }
-/**@callback requestCallback @param {emperReuqest} request @param {emperResponse} response*/
---
+++ emper.d.ts
@@ -0,0 +1,80 @@
+import { Server, IncomingMessage, ServerResponse } from "http"
+import * as net from "net"
+class App extends Server {
+    constructor(protocol: string, options: { insecureHTTPParser: boolean, maxHeaderSize: number })
+    listen(options: { port: number, hostname: string, backlog: number }, listeningListener: function(): void): void
+    delete(path: string, callback: requestCallback): void
+    get(path: string, callback: requestCallback): void
+    head(path: string, callback: requestCallback): void
+    options(path: string, callback: requestCallback): void
+    patch(path: string, callback: requestCallback): void
+    put(path: string, callback: requestCallback): void
+    /**Loads an object*/
+    loadApiRegister(register: { [path: string]: { bytes: number, counter: number } }, reset: boolean): void
+    /**"http(s)://${address}:${port}*/
+    get url(): string
+    get apis(): { [path: string]: ApiRecord }
+    static get IncomingMessage(): typeof Request
+    /**Set this value to null in order to reset it to the base Request class.*/
+    static set IncomingMessage(IncomingMessage: Request): void
+    static get ServerResponse(): typeof Response
+    /**Set this value to null in order to reset it to the base Response class.*/
+    static set ServerResponse(ServerResponse: Response): void
+    static get Socket(): typeof Socket
+    /**Set this value to null in order to reset it to the base Socket class.*/
+    static set Socket(Socket: Socket): void
+    static get ApiRecord(): typeof ApiRecord
+    /**The ApiRecord holds information on the api endpoint. Build in are a counter and bytes. The bytes represents te amount of bytes written from the server to sockets. Set this value to null in order to reset it to the base ApiRecord class.*/
+    static set ApiRecord(ApiRecord: ApiRecord): void
+    static get logger(): logger
+    static get mimetypes(): { [ext: string]: string }
+    /**Add mimetypes to the dictionary. The mimetypes enables detecting the content-type by the extension from a file and is used in the response's sendFile method*/
+    static set mimetypes(mimetypes: { [ext: string]: string }): void
+}
+type AppFactory = (protocol: string, options: {}) => typeof App
+type requestCallback = (request: Request, response: Response) => void
+class Request extends IncomingMessage {
+    socket: Socket
+    connection: Socket
+    body?: object
+    /**Contains key values identified in the url's path by /:key */
+    params?: { [key: string]: string }
+    static get bodyParsers(): RequestBodyParsers
+}
+class Response extends ServerResponse {
+    socket: Socket
+    connection: Socket
+    sendJson(status: number, data: object): void
+    send(data: Buffer): void
+    send(data: string): void
+    report(byteLength: number): Response
+    sendError(status: number, error: Error): void
+    sendFile(filepath: string, end: boolean): Response
+    get apiRecord(): void
+}
+class Socket extends net.Socket {
+    get hrtimeAlive(): [number, number]
+    get msTimeAlive(): number
+    get remoteUrl(): string
+}
+class ApiRecord {
+    bytes: number
+    counter: number
+    /**Copies record's counter and bytes properties, if falsy sets them to 0*/
+    from(record: object): void
+    /**Sets counter and bytes to 0*/
+    reset(): ApiRecord
+}
+type logger = { error: log, log: log }
+type log = (...data) => any
+class RequestBodyParsers {
+    ["application/json"]: RequestBodyParser
+    ["application/x-www-form-urlencoded"]: RequestBodyParser
+    [x: string]: RequestBodyParser
+    add(mimetype: string, parse: parse, errorMessage: string): RequestBodyParsers
+}
+type parse = (body: string) => object
+class RequestBodyParser {
+    parse(body: string, request: Request, onParsed: onParsed): void
+}
+type onParsed = (parsed: object) => void
